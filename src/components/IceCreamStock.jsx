22adbba4feab6e6ab9f09ed8f58117a72eb8047d
bhallaarjun <bhalla.arjun@gmail.com>
2025-06-09 18:58:00 +0100

    Mock data coming from server

--- src/components/IceCreamStock.jsx
+++ src/components/IceCreamStock.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import IceCreamList from "./IceCreamList";
 import SearchBar from "./SearchBar";
 import AddIceCream from "./AddIceCream";
@@ -6,127 +6,132 @@ import AddIceCream from "./AddIceCream";
 const IceCreamStock = () => {
   const [searchText, setSearchText] = useState("");
   const [inStockOnly, setInStockOnly] = useState(false);
-  const [iceCreams, setIceCreams] = useState([
-    {
-      category: "420ML Tub",
-      quantity: 3,
-      brand: "Haagen-Dazs",
-      flavour: "Mint Chocolate",
-      stocked: true,
-    },
-    {
-      category: "420ML Tub",
-      quantity: 1,
-      brand: "Haagen-Dazs",
-      flavour: "Lotus Biscoff Biscuit",
-      stocked: true,
-    },
-    {
-      category: "420ML Tub",
-      quantity: 1,
-      brand: "Haagen-Dazs",
-      flavour: "Coffee",
-      stocked: true,
-    },
-    {
-      category: "420ML Tub",
-      quantity: 1,
-      brand: "Haagen-Dazs",
-      flavour: "Macadamia Nut Brittle",
-      stocked: true,
-    },
-    {
-      category: "420ML Tub",
-      quantity: 1,
-      brand: "Haagen-Dazs",
-      flavour: "Dulce de Leche",
-      stocked: true,
-    },
-    {
-      category: "420ML Tub",
-      quantity: 1,
-      brand: "Haagen-Dazs",
-      flavour: "Red Velvet",
-      stocked: true,
-    },
-    {
-      category: "420ML Tub",
-      quantity: 1,
-      brand: "Haagen-Dazs",
-      flavour: "Strawberries & Cream",
-      stocked: true,
-    },
-    {
-      category: "420ML Tub",
-      quantity: 1,
-      brand: "Haagen-Dazs",
-      flavour: "Coffee",
-      stocked: true,
-    },
-    {
-      category: "460ML Tub",
-      quantity: 2,
-      brand: "Jude's",
-      flavour: "Blood Orange",
-      stocked: true,
-    },
-    {
-      category: "460ML Tub",
-      quantity: 1,
-      brand: "Jude's",
-      flavour: "Belgian Chocolate",
-      stocked: true,
-    },
-    {
-      category: "460ML Tub",
-      quantity: 1,
-      brand: "Jude's",
-      flavour: "Cherries & Cream",
-      stocked: true,
-    },
-    {
-      category: "460ML Tub",
-      quantity: 1,
-      brand: "Jude's",
-      flavour: "Mint Chocolate",
-      stocked: true,
-    },
-    {
-      category: "460ML Tub",
-      quantity: 2,
-      brand: "Grom",
-      flavour: "Pistachio",
-      stocked: true,
-    },
-    {
-      category: "100ML Stick",
-      quantity: 6,
-      brand: "Magnum",
-      flavour: "Dark Chocolate",
-      stocked: true,
-    },
-    {
-      category: "1L Tub",
-      quantity: 1,
-      brand: "M&S",
-      flavour: "Cornish Vanilla",
-      stocked: true,
-    },
-    {
-      category: "2L Tub",
-      quantity: 1,
-      brand: "M&S",
-      flavour: "Cornish Vanilla",
-      stocked: true,
-    },
-    {
-      category: "2L Tub",
-      quantity: 0, // out of stock item
-      brand: "M&S",
-      flavour: "Chocolate",
-      stocked: false,
-    },
-  ]);
+  const [iceCreams, setIceCreams] = useState([]);
+
+  // Mock function simulating feching data from server
+  const fetchIceCreams = async () => {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve([
+          {
+            category: "420ML Tub",
+            quantity: 4,
+            brand: "Haagen-Dazs",
+            flavour: "Mint Chocolate",
+            stocked: false,
+          },
+          {
+            category: "420ML Tub",
+            quantity: 1,
+            brand: "Haagen-Dazs",
+            flavour: "Lotus Biscoff Biscuit",
+            stocked: true,
+          },
+          {
+            category: "420ML Tub",
+            quantity: 1,
+            brand: "Haagen-Dazs",
+            flavour: "Coffee",
+            stocked: true,
+          },
+          {
+            category: "420ML Tub",
+            quantity: 0,
+            brand: "Haagen-Dazs",
+            flavour: "Macadamia Nut Brittle",
+            stocked: true,
+          },
+          {
+            category: "420ML Tub",
+            quantity: 0, // out of stock item
+            brand: "Haagen-Dazs",
+            flavour: "Dulce de Leche",
+            stocked: true,
+          },
+          {
+            category: "420ML Tub",
+            quantity: 0, // out of stock item
+            brand: "Haagen-Dazs",
+            flavour: "Red Velvet",
+            stocked: true,
+          },
+          {
+            category: "420ML Tub",
+            quantity: 0, // out of stock item
+            brand: "Haagen-Dazs",
+            flavour: "Strawberries & Cream",
+            stocked: true,
+          },
+          {
+            category: "460ML Tub",
+            quantity: 2,
+            brand: "Jude's",
+            flavour: "Blood Orange",
+            stocked: true,
+          },
+          {
+            category: "460ML Tub",
+            quantity: 1,
+            brand: "Jude's",
+            flavour: "Belgian Chocolate",
+            stocked: true,
+          },
+          {
+            category: "460ML Tub",
+            quantity: 4,
+            brand: "Jude's",
+            flavour: "Cherries & Cream",
+            stocked: true,
+          },
+          {
+            category: "460ML Tub",
+            quantity: 0, // out of stock item
+            brand: "Jude's",
+            flavour: "Mint Chocolate",
+            stocked: true,
+          },
+          {
+            category: "460ML Tub",
+            quantity: 1,
+            brand: "Grom",
+            flavour: "Pistachio",
+            stocked: true,
+          },
+          {
+            category: "100ML Stick",
+            quantity: 6,
+            brand: "Magnum",
+            flavour: "Dark Chocolate",
+            stocked: true,
+          },
+          {
+            category: "1L Tub",
+            quantity: 1,
+            brand: "M&S",
+            flavour: "Cornish Vanilla",
+            stocked: true,
+          },
+          {
+            category: "2L Tub",
+            quantity: 1,
+            brand: "M&S",
+            flavour: "Cornish Vanilla",
+            stocked: true,
+          },
+        ]);
+      }, 3000); // simulate a delay
+    });
+  };
+
+  // useEffect to fetch data when the component mounts
+  useEffect(() => {
+    const getIceCreams = async () => {
+      const data = await fetchIceCreams();
+      setIceCreams(data);
+      // console.log("geticecreams");
+    };
+    getIceCreams();
+  }, []); // Empty dependency array to ensure this runs only once
 
   // Function to handle adding a new ice cream to the stock when the
   // 'new entry' button is clicked
